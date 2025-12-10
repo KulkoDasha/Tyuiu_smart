d57b42bb20f7e12faf63a6f020554ba19226b9b0
KulkoDasha <kulko.dasha.2006@gmail.com>
2025-12-10 11:34:47 +0500

    Ура

--- config/config.py
+++ config/config.py
@@ -1,1 +1,39 @@
 #файл конфигурации
+from dataclasses import dataclass
+from dotenv import load_dotenv
+from pathlib import Path
+import os
+    
+@dataclass
+class LogSettings:
+    level: str    
+    format: str  
+
+@dataclass
+class TgBot:
+    token: str
+    
+@dataclass
+class Config:
+    bot: TgBot
+    log: LogSettings
+
+def load_config(path: str | None = None) -> Config:
+    """создает парсер(скрипт который собирает и структурирует данные), 
+        считывает переменные из файла .env и заполняет их"""
+    if path is None:
+        env_path = str(Path(__file__).parent.parent / ".env")
+    else:
+        env_path = path
+    load_dotenv(dotenv_path=path, override=True)
+    token = os.getenv("BOT_TOKEN")
+    if token is None:
+        raise ValueError(f"BOT_TOKEN не найден в файле {env_path}")
+    return Config(
+        bot=TgBot(token=token),
+        log=LogSettings(
+            level=os.getenv("LOG_LEVEL", "INFO"), 
+            format=os.getenv("LOG_FORMAT", "{asctime} - {levelname} - {name} - {message}")
+        ))
+    
+config = load_config()
---
+++ main.py
@@ -0,0 +1,35 @@
+import asyncio
+import logging
+
+from aiogram import Bot, Dispatcher
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+import shutil
+from pathlib import Path
+
+from config.config import config
+
+logging.basicConfig(
+    level=logging.getLevelName(config.log.level),
+    format=config.log.format,
+    style='{'
+)
+logger = logging.getLogger(__name__)
+
+async def main():
+    session_path = Path("session")
+    if session_path.exists():
+        shutil.rmtree(session_path)
+        print("Старые сессии удалены")
+        logger.info("Старые сессии удалены")
+    logger.info(f"Загружен полный токен: {config.bot.token}")
+    bot = Bot(
+        token=config.bot.token,
+        default=DefaultBotProperties(parse_mode=ParseMode.HTML)
+    )
+    dp = Dispatcher()
+    await bot.delete_webhook(drop_pending_updates=True) 
+    await dp.start_polling(bot)
+    
+if __name__ == "__main__":
+    asyncio.run(main())
